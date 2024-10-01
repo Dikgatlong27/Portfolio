9dcfa7b25e4bbc2f8f140965a03aaf8f146e7ca9
Tebogo Dikgatlong <tpdikgatlong@gmail.com>
2024-10-01 17:48:33 +0200

    Build Folder

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFileDownload } from "react-icons/fa";
 
 import './Hero.css'
 
@@ -7,11 +8,11 @@ const Hero = () => {
     <div className='container'>
         <div className="content">
         <p className='my-name'>Tebogo Dikgatlong</p>
-        <h1 style={{color: '#00b132'}}><span style={{color: '#333'}}>I'm a</span> Web Developer</h1>
-        <p>Experienced frontend developer with passion for creating virtually stunning and user-friendly websites</p>
+        <h1 style={{color: '#00b132', fontSize: '5vmax'}}><span style={{color: '#333'}}>I'm a</span> Web Developer</h1>
+        <p style={{textAlign: 'start'}}>Experienced frontend developer with passion for creating virtually stunning and user-friendly websites</p>
         <div className="hero-buttons">
-            <a className='hire-me' href="https://wa.me/+27676228529" target="_blank" rel="noopener noreferrer">Hire Me</a>
-            <a className='resume' href='/resume.pdf' download="resume.pdf"><button>Download CV</button></a>
+            <a className='hire-me' href="https://wa.me/27676228529" target="_blank" rel="noopener noreferrer">Hire Me</a>
+            <a className='resume' href='/resume.pdf' download="resume.pdf">Download CV <FaFileDownload /></a>
         </div> 
         </div>
       
